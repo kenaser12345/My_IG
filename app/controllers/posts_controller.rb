65696c1ab32c94629bef4eecd10d552aa0cf3fe9
kenaser12345 <kenaser12345@gmail.com>
2020-09-10 18:01:33 +0800

    add navbar public and account

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.new(post_params)
+    @post = current_account.posts.build(post_params)
 
     if @post.save!
       redirect_to dashboard_path, flash: { success: "Post was created successfully!!"}
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
-
+  before_create :set_active
+  belongs_to :account
   scope :active,->{ where(:active => true) }
+
+  def set_active
+    self.active = true
+  end
 end
